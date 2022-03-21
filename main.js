d1fac8499c7a41e1b0e7c5866b0cc7fb92f782e8
galaxykawaii57 <69598262+galaxykawaii57@users.noreply.github.com>
2022-03-21 15:52:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Recognition = new SpeechRecognition();
+function start()
+{
+    document.getElementById("textbox").innerHTML="";
+    Recognition.start();
+}
+Recognition.onresult=function(event){
+    console.log(event);
+    var content=event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML=content;
+    console.log(content);
+    speak();
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value;
+    var utterthis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+}
+
+camera=document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'jpeg',
+    jpeg_quality:90
+});
